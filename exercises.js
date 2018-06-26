192722a3f4e13ec61051145a26adf087ef236c2f
Charlie Sparks <csparks@antioch.edu>
2018-06-26 14:04:25 -0700

    started by adding notes on syntax and general exercises

---
+++ exercises.js
@@ -0,0 +1,11 @@
+// # problem 1 
+//instructions: given an array with the values [1, 2, 3], use a for loop to log out each of the values sequentially. This is for the purpose of showing how iteration works without diving into methods.
+
+// # problem 2
+//instructions: given an array with the values of [1, 2, 3], use the .forEach method to log out each of the values sequentially
+
+// # problem 3
+//instructions: given an array with the values [1, 2, 3], create a new array that stores all of the values +1. This requires the use of the .map method. After you have created the new array, log out the values of the new array using .forEach
+
+//# problem 4
+//instructions: given an array with the values [1, 2, 3], use the .reduce method to store the result of adding all the values together. 
---
+++ methods.js
@@ -0,0 +1,23 @@
+// # general method syntax:
+arrayName.methodName((parameters) => {
+    code to execute
+  })
+
+// # forEach
+//with a normal function
+arrayName.forEach(function(item, index){
+    code to execute
+})
+
+//with an arrow function
+arrayName.forEach((item, index) => {code to execute})
+
+// # map
+//normal function syntax
+arrayName.map(function(item){
+    return code to execute
+})
+
+//arrow function
+arrayName.map((item) => code to execute);
+
